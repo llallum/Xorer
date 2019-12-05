12a64290cee90a2df9efe638379dc8955ea6d192
Llallum Victoria <llallumvictoria@yahoo.com>
2019-12-06 02:57:41 +0800

    Updated the comments

--- decrypt_xor.py
+++ decrypt_xor.py
@@ -1,14 +1,14 @@
-fHandle = open("G:\\_\\case\\wbtemp_16097931245830854983160605385390868\\2a7c5618b666132ee8bd8adf4a05846ca5341e2d", "rb")
+fHandle = open("This is the file to be decrypted", "rb")
 
 array = bytearray(fHandle.read())
 
 temp = array[:]
 
 
-key = bytearray("\x4A\x73\x55\x2F\x55\x47\x55\x6F\x7D\x4A")
+key = bytearray("Here is the key")
 
 
-for a in range(0, len(array), 10):
+for a in range(0, len(array), 10): # 10 is just an example of key length
     for i in range(0, len(key)):
         if (i+a)>= len(array):
             break;
